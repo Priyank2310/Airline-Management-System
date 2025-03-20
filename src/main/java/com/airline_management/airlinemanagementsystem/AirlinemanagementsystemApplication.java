d875f7f3764fc9bc449f5041d54f5562900c3630
Priyank Maheshwari <priyankmaheshwari23@gmail.com>
2025-03-20 21:50:21 +0530

    Initial commit : Setup for Airline Management System

---
+++ src/main/java/com/airline_management/airlinemanagementsystem/AirlinemanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.airline_management.airlinemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinemanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinemanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airline_management/airlinemanagementsystem/AirlinemanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline_management.airlinemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlinemanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
